6e1941e1025db585ba5732acab2ebd33c7f7bde9
Zachary Stence <zachstence@gmail.com>
2021-11-20 10:41:32 -0600

    feat: landing page

---
+++ src/lib/components/Social.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    export let icon: string;
+    export let alt: string;
+    export let content: string;
+    export let link: string;
+</script>
+
+<a href={link}>
+    <img src={icon} {alt} />
+    <span>{content}</span>
+</a>
+
+
+<style lang="postcss">
+    a {
+        @apply flex items-center 
+            m-2 p-2
+            box-border w-auto
+            border-2 border-transparent rounded-md;
+        width: fit-content;
+    }
+
+    a:hover {
+        @apply border-white;
+    }
+
+
+    img {
+        @apply w-8 h-8 mr-4;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Social from "$lib/components/Social.svelte";
+
+    const socials = [
+        {
+            icon: "GitHub-Mark-Light-120px-plus.png",
+            alt: "GitHub",
+            content: "github.com/zachstence",
+            link: "https://www.github.com/zachstence",
+        },
+        {
+            icon: "LI-In-Bug.png",
+            alt: "LinkedIn",
+            content: "linkedin.com/in/zachary-stence",
+            "link": "https://www.linkedin.com/in/zachary-stence",
+        },
+        {
+            icon: "icons8-email-64.png",
+            alt: "Email",
+            content: "zachstence@gmail.com",
+            link: "mailto:zachstence@gmail.com",
+        },
+    ];
+</script>
+
+
+<main>
+    <img src="https://avatars.githubusercontent.com/u/31896377?v=4" alt="Profile" />
+    <h1>Zachary Stence</h1>
+    <div>
+        {#each socials as social}
+            <Social {...social} />
+        {/each}
+    </div>
+</main>
+
+
+<style lang="postcss">
+    img {
+        @apply w-96 h-96 rounded-full;
+    }
+
+    h1 {
+        @apply text-5xl mt-8 mb-8;
+    }
+
+    main {
+        @apply h-screen flex flex-col items-center justify-center;
+    }
+
+    div {
+        @apply border-2 border-white rounded-md;
+    }
+</style>
